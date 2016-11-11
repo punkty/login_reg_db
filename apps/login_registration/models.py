217ee61b2f14a21c964b4fb039b52adb1666c343
Ben Swanson <bentswanson@gmail.com>
2016-11-11 14:55:30 -0800

    sets up views, urls, htmls, model

--- apps/login_registration/models.py
+++ apps/login_registration/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 50)
+    password = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- apps/login_registration/views.py
+++ apps/login_registration/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 def index(request):
     return render(request, 'login_registration/index.html')
+
+def process(request):
+
+    return redirect('/')
+
+    return render(request, 'login_registration/success.html')
